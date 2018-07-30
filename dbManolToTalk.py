de2231760892a20f8afb4b845013946b36d8c08f
kyriegorrin <kyriegorrin@gmail.com>
2018-07-30 14:54:59 +0200

    GUI basica feta, falta logica

--- dbManolToTalk.py
+++ dbManolToTalk.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from tkinter import ttk
+from tkinter import ttk, font
 
 #GUI part
 class AppGUI():
@@ -12,21 +12,39 @@ class AppGUI():
         window.title("dbManolToTalk")
         
         #Miscellaneous variables
-        imanolOriginal = PhotoImage(file="imanol.png")
-        imanol = imanolOriginal.subsample(2)
+        self.imanolOriginal = PhotoImage(file="imanol.png")
+        self.imanol = self.imanolOriginal.subsample(2)
+        
+        self.myFont = font.Font(weight="bold")
+
+        self.sens = DoubleVar()
 
         #Widget definition
         #TODO: commands for buttons
         self.talkKey = ttk.Button(window, text = "KEY BIND: V")
         self.stopKey = ttk.Button(window, text = "STOP KEY: F5")
 
+        self.sensLabel = ttk.Label(window, text="Sensitivity", 
+                                    font=self.myFont, background="white")
+
+        self.slider = ttk.Scale(window, 
+                                variable = self.sens, 
+                                orient = HORIZONTAL,
+                                length=250,
+                                from_=0.0,
+                                to=100.0)
+
         self.canvas = Canvas(window, width=150, height=150, bg="white")
 
         #Widget configuration
-        self.canvas.create_image(75, 75, image=imanol, anchor=CENTER)
+        self.canvas.create_image(75, 75, image=self.imanol, anchor=CENTER)
 
         #Widget placement
-        self.canvas.pack(side=LEFT)
+        self.canvas.place(x=10, y=25)
+        self.sensLabel.place(x=200, y=50)
+        self.slider.place(x=200, y=75)
+        self.talkKey.place(x=250, y=150)
+        self.stopKey.place(x=350, y=150)
 
         #Whatever the fuck this is
         window.mainloop()
